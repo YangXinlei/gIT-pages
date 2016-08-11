589af85f4da60a49eef9f58fa5d3bf8fb01b4a83
yangxinlei <yangxinlei007@live.com>
2016-08-11 19:44:49 +0800

    添加三个vc

--- gIT pages/ActivityViewController.m
+++ gIT pages/ActivityViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "ActivityViewController.h"
+#import "HomeViewController.h"
+#import "PostDetailViewController.h"
+#import "CommentsViewController.h"
+#import "PostItem.h"
+#import "PostComment.h"
 #import "ToggleView.h"
 
 @interface ActivityViewController () <ToggleViewDelegate>
+
+@property (nonatomic, strong) NSMutableArray       *vcArray;
+@property (nonatomic, assign) NSInteger             curIndex;
 
 @end
 
@@ -26,7 +34,23 @@
     UIView *titleView = [[ToggleView alloc] initWithNames:barTitles delegate:self];
     
     [[self navigationItem] setTitleView:titleView];
-
+    
+    _vcArray = [NSMutableArray arrayWithCapacity:[barTitles count]];
+    HomeViewController *homeVC = [[HomeViewController alloc] init];
+    [_vcArray addObject:homeVC];
+    
+    PostItem *item = [[PostItem alloc] initWithTitle:@"abc" Url:@"https://yangxinlei.github.io/ios/2016/07/25/UIScrollView-contentSize-contentOffset-contentInset.html" Blogger:nil Twitter:nil Tag:nil Comments:nil Likes:0 Date:0];
+    PostDetailViewController *hotPostVC = [[PostDetailViewController alloc] initWithPostItem:item];
+    [_vcArray addObject:hotPostVC];
+    
+    PostComment *comment1 = [[PostComment alloc] initWithPost:nil Blogger:nil Content:@"lz我爱你" Date:@"2016.8"];
+    PostComment *comment2 = [[PostComment alloc] initWithPost:nil Blogger:nil Content:@"这篇文章不错" Date:@"2016.3"];
+    NSArray<PostComment *> *commentArray = @[comment1, comment2];
+    CommentsViewController *commentMeVC = [[CommentsViewController alloc] initWithComments:commentArray];
+    [_vcArray addObject:commentMeVC];
+    
+    [self loadViewAtIndex:0];
+    _curIndex = 0;
 }
 
 -(void)didReceiveMemoryWarning
@@ -36,7 +60,22 @@
 
 - (void)toggleView:(ToggleView *)view didToggleToIndex:(NSInteger)index
 {
-    NSLog(@"%s", __FUNCTION__);
+    [self loadViewAtIndex:index];
+    _curIndex = index;
+}
+
+- (void)loadViewAtIndex:(NSInteger)index
+{
+    UIViewController *vc = _vcArray[index];
+    UIViewController *curVC = _vcArray[_curIndex];
+    
+    [curVC.view removeFromSuperview];
+    [curVC removeFromParentViewController];
+    curVC = nil;
+    
+    [self.view addSubview:vc.view];
+    [self addChildViewController:vc];
+    [vc didMoveToParentViewController:self];
 }
 
 @end
